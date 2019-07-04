685818ea24f5b9e7d6995cf90044ab9d5fd30259
bzlldhszsfx <45312994+bzlldhszsfx@users.noreply.github.com>
2019-07-04 21:10:11 +0800

    Add files via upload

---
+++ 数字显示（原）/数字显示（原）.ino
@@ -0,0 +1,108 @@
+void setup()
+{
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  char income; 
+  if(Serial.available()>0)
+  {
+    income=Serial.read();
+    if(income=='0')
+      {
+        digitalWrite(9,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(8,LOW);
+        digitalWrite(9,HIGH);
+      }
+    if(income=='1')
+      {
+        digitalWrite(9,LOW);
+        digitalWrite(5,HIGH);
+        digitalWrite(6,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(8,LOW);
+        digitalWrite(9,HIGH);
+      }
+    if(income=='2')
+      {
+        digitalWrite(9,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(6,HIGH);
+        digitalWrite(7,LOW);
+        digitalWrite(8,LOW);
+        digitalWrite(9,HIGH);
+      }
+    if(income=='3')
+      {
+        digitalWrite(9,LOW);
+        digitalWrite(5,HIGH);
+        digitalWrite(6,HIGH);
+        digitalWrite(7,LOW);
+        digitalWrite(8,LOW);
+        digitalWrite(9,HIGH);
+      }
+    if(income=='4')
+      {
+        digitalWrite(9,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(7,HIGH);
+        digitalWrite(8,LOW);
+        digitalWrite(9,HIGH);
+      }
+    if(income=='5')
+      {
+        digitalWrite(9,LOW);
+        digitalWrite(5,HIGH);
+        digitalWrite(6,LOW);
+        digitalWrite(7,HIGH);
+        digitalWrite(8,LOW);
+        digitalWrite(9,HIGH);
+      }
+    if(income=='6')
+      {
+        digitalWrite(9,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(6,HIGH);
+        digitalWrite(7,HIGH);
+        digitalWrite(8,LOW);
+        digitalWrite(9,HIGH);
+      }
+    if(income=='7')
+      {
+        digitalWrite(9,LOW);
+        digitalWrite(5,HIGH);
+        digitalWrite(6,HIGH);
+        digitalWrite(7,HIGH);
+        digitalWrite(8,LOW);
+        digitalWrite(9,HIGH);
+      }
+    if(income=='8')
+      {
+        digitalWrite(9,LOW);
+        digitalWrite(5,LOW);
+        digitalWrite(6,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(8,HIGH);
+        digitalWrite(9,HIGH);
+      }
+    if(income=='9')
+      {
+        digitalWrite(9,LOW);
+        digitalWrite(5,HIGH);
+        digitalWrite(6,LOW);
+        digitalWrite(7,LOW);
+        digitalWrite(8,HIGH);
+        digitalWrite(9,HIGH);
+      }
+  } 
+}
